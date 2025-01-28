93328d34cf8ab7977ea301cc28db1cbe909aac8a
Pritam Roy <ratsdust4226@gmail.com>
2025-01-28 22:31:30 +0530

    speech to text

--- src/hooks/useChatAPI.ts
+++ src/hooks/useChatAPI.ts
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import { GoogleGenerativeAI } from "@google/generative-ai";
 import { Message } from "../components/ChatBox";
 import { speak } from "../utils/tts";
+import { generateBotReply } from "../services/generativeAIService";
 
 const useChatAPI = () => {
   const [messages, setMessages] = useState<Message[]>([]);
-
 
   const sendMessage = async (userMessage: string): Promise<void> => {
     const updatedMessages: Message[] = [
@@ -15,30 +14,12 @@ const useChatAPI = () => {
     setMessages(updatedMessages);
 
     try {
-      const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_API_KEY);
-      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-      
+      const reply = await generateBotReply(updatedMessages, userMessage);
 
-      // Combine messages into context
-      const context = updatedMessages
-        .map((msg) => `${msg.sender === "user" ? "User" : "Bot"}: ${msg.text}`)
-        .join("\n");
-
-      // Send the userMessage with context
-      const result = await model.generateContent(`Keep the response short.\n${context}\nUser: ${userMessage}\nBot:`,
-      );
-
-      const response = result.response;
-
-      if (!response) {
-        throw new Error("No response received.");
-      }
-
-      const reply = response.text() || "No response received.";
-      if(reply!=""&& reply !== "No response received."){
+      if (reply && reply !== "No response received.") {
         speak(reply);
       }
-      console.log("Bot reply:", reply);
+
       setMessages((prev) => [...prev, { sender: "bot", text: reply }]);
     } catch (error) {
       console.error("Error sending message:", error);
