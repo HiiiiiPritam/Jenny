e434013a8ae6eac0dea6aca95c277c82e587940b
Pritam Roy <ratsdust4226@gmail.com>
2025-03-14 16:24:53 +0530

    personality imorovedd

---
+++ src/app/dashboard/video/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='bg-black h-full w-full text-white text-center text-2xl'>
+      <h1>Sorry, This service is currently unavailable</h1>
+    </div>
+  )
+}
+
+export default page
